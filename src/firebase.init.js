c07cbb4bb36e20960701bf475bc24191d51c561b
Shakil53 <shakilahmed.dcc@gmail.com>
2023-03-11 12:31:16 +0600

    firebase auth with google sign in and sign out button

---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBwShJnbgmlZj6ayJQefwd_ARxdIvj40P4",
+    authDomain: "timple-firebase-auth-aed55.firebaseapp.com",
+    projectId: "timple-firebase-auth-aed55",
+    storageBucket: "timple-firebase-auth-aed55.appspot.com",
+    messagingSenderId: "594057789923",
+    appId: "1:594057789923:web:9226c3e46814794cbe20d6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
